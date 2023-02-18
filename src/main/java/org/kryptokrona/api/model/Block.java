bd35bb82682f0465dd4932bc09207f698b787d12
mjovanc <mjovanc@icloud.com>
2023-02-19 00:44:38 +0100

    Adding properties in Block model

--- src/main/java/org/kryptokrona/api/model/Block.java
+++ src/main/java/org/kryptokrona/api/model/Block.java
@@ -1,8 +1,12 @@
 package org.kryptokrona.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 /**
  * Block entity.
@@ -14,5 +18,25 @@ public class Block {
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+    private Long time;
+
+    private float difficulty;
+
+    private float reward;
+
+    @JsonProperty("total_fees")
+	@Column(name = "total_fees")
+    private Long totalFees;
+
+    @JsonProperty("block_size")
+	@Column(name = "block_size")
+    private Long blockSize;
+
+    @JsonProperty("previous_hash")
+	@Column(name = "previous_hash")
+    private String previousHash;
+
+    private List<Transaction> transactions;
 
 }

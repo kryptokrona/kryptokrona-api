f54a39ad5f65f391942c3323001ff67923b25663
mjovanc <mjovanc@icloud.com>
2023-02-24 22:05:05 +0100

    Adding OpenAPI specification

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,9 @@ dependencies {
     implementation("io.ktor:ktor-serialization-jackson-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    runtimeOnly("com.squareup:kotlinpoet:0.7.0")
+
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/org/kryptokrona/plugins/Routing.kt
+++ src/main/kotlin/org/kryptokrona/plugins/Routing.kt
@@ -33,9 +33,12 @@ package org.kryptokrona.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.swagger.*
 
 fun Application.configureRouting() {
     routing {
+        swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
+
         get("/") {
             call.respondText("Hello World!")
         }

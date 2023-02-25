921b8ecdd201293e7960fbb586f6699c28a71450
mjovanc <mjovanc@icloud.com>
2023-02-26 00:50:22 +0100

    Adding new run configuration for migrations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val postgres_version: String by project
 val h2_version: String by project
 val ktorm_version: String by project
+val liquibase_core: String by project
 
 plugins {
+    application
     kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
     id("war")
+    id("org.liquibase.gradle") version "2.1.1"
 }
 
 group = "org.kryptokrona"
@@ -27,6 +33,7 @@ repositories {
 }
 
 dependencies {
+    // various
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
@@ -38,16 +45,60 @@ dependencies {
     implementation("org.ktorm:ktorm-core:$ktorm_version")
     implementation("io.ktor:ktor-server-config-yaml-jvm:2.2.3")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.liquibase:liquibase-core")
 
-    //TODO: add liquibase
+    // liquibase
+    liquibaseRuntime("org.liquibase:liquibase-core:$liquibase_core")
+    liquibaseRuntime("ch.qos.logback:logback-core:1.2.3")
+    liquibaseRuntime("ch.qos.logback:logback-classic:1.2.3")
+    liquibaseRuntime("javax.xml.bind:jaxb-api:2.2.4")
 
     runtimeOnly("com.squareup:kotlinpoet:0.7.0")
 
+    // testing
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
 tasks.war {
     archiveFileName.set("kryptokrona-api.war")
+}
+
+// database migrations
+val dbEnv: String by project.extra { "" }
+
+val propertiesFile = file("local.properties")
+val properties = Properties()
+if (propertiesFile.exists()) {
+    properties.load(propertiesFile.inputStream())
+}
+
+val urlDev = properties.getProperty("liquibase.dev.url") ?: System.getenv("LIQUIBASE_DEV_URL")
+val userDev = properties.getProperty("liquibase.dev.user") ?: System.getenv("LIQUIBASE_DEV_USER")
+val pwdDev = properties.getProperty("liquibase.dev.password") ?: System.getenv("LIQUIBASE_DEV_PASSWORD")
+
+liquibase {
+    activities.register("dev") {
+        this.arguments = mapOf(
+            "logLevel" to "info",
+            "changeLogFile" to "src/main/resources/db/migration/migrations.xml",
+            "url" to urlDev,
+            "username" to userDev,
+            "password" to pwdDev
+        )
+    }
+
+    activities.register("prod") {
+        val url = System.getenv("LIQUIBASE_URL")
+        val user = System.getenv("LIQUIBASE_USER")
+        val pwd = System.getenv("LIQUIBASE_PASSWORD")
+
+        this.arguments = mapOf(
+            "logLevel" to "info",
+            "changeLogFile" to "/resources/db/migration/migrations.xml",
+            "url" to url,
+            "username" to user,
+            "password" to pwd
+        )
+    }
+    runList = dbEnv
 }

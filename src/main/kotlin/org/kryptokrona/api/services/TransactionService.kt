89d1289a8bad1632dcc11b4031c2bec6d3165d14
mjovanc <mjovanc@icloud.com>
2023-03-04 22:54:27 +0100

    Adding service interfaces

--- src/main/kotlin/org/kryptokrona/api/services/TransactionService.kt
+++ src/main/kotlin/org/kryptokrona/api/services/TransactionService.kt
@@ -28,6 +28,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-package org.kryptokrona.api.models
+package org.kryptokrona.api.services
 
-//TODO to be defined what we should store here.
+interface TransactionService {
+}

7b49f1f403cffdc0b6097bef766d9b7841be0646
mjovanc <mjovanc@icloud.com>
2023-03-04 23:43:47 +0100

    Implementing new services

--- src/main/kotlin/org/kryptokrona/api/services/SupplyServiceImpl.kt
+++ src/main/kotlin/org/kryptokrona/api/services/SupplyServiceImpl.kt
@@ -1,4 +1,38 @@
 package org.kryptokrona.api.services
 
-class SupplyServiceImpl {
+import org.kryptokrona.api.models.*
+import org.kryptokrona.api.plugins.DatabaseConnection
+import org.ktorm.dsl.*
+import org.ktorm.entity.add
+import org.ktorm.entity.count
+import org.ktorm.entity.find
+
+class SupplyServiceImpl : SupplyService {
+
+    private val db = DatabaseConnection.database
+
+    override fun getAll(size: Int, page: Int): List<Supply> {
+        return db.from(Supplies)
+            .select()
+            .offset((page - 1) * size)
+            .limit(size)
+            .map { row -> Supplies.createEntity(row) }
+    }
+
+    override fun getById(id: Long): Supply? {
+        return db.supplies.find { it.id eq id }
+    }
+
+    override fun save(supply: Supply) {
+        db.supplies.add(supply)
+    }
+
+    override fun delete(id: Long) {
+        db.delete(Supplies) { it.id eq id }
+    }
+
+    override fun getTotalCount(): Int {
+        return db.supplies.count()
+    }
+
 }

1bb4aa25af248b0e7ed9d10da2e4ee62d038270d
mjovanc <mjovanc@icloud.com>
2023-03-12 11:27:10 +0100

    Updating docker compose and scripts submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,11 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 tasks.withType<KotlinCompile>().all {
     kotlinOptions {
         jvmTarget = "17"

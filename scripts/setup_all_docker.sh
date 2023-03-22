60f315de138245ac6d4e37da56cd26b7db43a2db
mjovanc <mjovanc@icloud.com>
2023-03-22 12:22:57 +0100

    Update script to add hostname for keycloak to OS

--- scripts/setup_all_docker.sh
+++ scripts/setup_all_docker.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# inject keycloak hostname
+sudo -- sh -c -e "echo '127.0.0.1   keycloak' >> /etc/hosts"
+
 # start docker compose with all services
 docker-compose -f ../docker-compose.yml up -d
--- scripts/setup_backend_docker.sh
+++ scripts/setup_backend_docker.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# inject keycloak hostname
+sudo -- sh -c -e "echo '127.0.0.1   keycloak' >> /etc/hosts"
+
 # start docker compose with backend (used for developing the monitor)
 docker-compose -f ../docker-compose.yml up -d db liquibase backend prometheus keycloak

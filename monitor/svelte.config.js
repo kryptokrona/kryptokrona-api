bbd21b9eacf770ce5865b2eefac7c8d3cb607857
mjovanc <mjovanc@icloud.com>
2023-04-10 16:09:04 +0200

    Updating sveltekit asset path

--- monitor/svelte.config.js
+++ monitor/svelte.config.js
@@ -22,9 +22,9 @@ const config = {
     handler(warning);
   },
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    paths: {
+      assets: '/monitor/_app/'
+    },
     adapter: adapter(),
   },
 };

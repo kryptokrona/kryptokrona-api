fbf06e9986c237d3756142de7577fe24f345bfb5
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2023-04-12 11:46:22 +0200

    Fixed issue

--- monitor/src/api/prometheus.js
+++ monitor/src/api/prometheus.js
@@ -11,17 +11,6 @@ export async function getCpuUsage() {
     console.error(error);
   }
 }
-export async function getCpuUsage() {
-  try {
-    const response = await fetch(
-      endpoint + "/query?query=rate(process_cpu_seconds_total[30s])"
-    );
-    const data = await response.json();
-    return data.data.result[0].value[1] * 100;
-  } catch (error) {
-    console.error(error);
-  }
-}
 export async function getramUsage() {
   try {
     const response = await fetch(

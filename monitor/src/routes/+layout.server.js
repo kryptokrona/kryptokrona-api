647bd150c7da05c9a9568d1ef8156a136c0883dc
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2023-04-12 23:03:02 +0200

    Added CPU usage over time (#130)

--- monitor/src/routes/+layout.server.js
+++ monitor/src/routes/+layout.server.js
@@ -1,4 +1,7 @@
 export const ssr = true;
+
+import { getPosts } from "../api/hugin";
+import { getThreads, getCpuUsage, getCpuUsage1h, getramUsage, getUptime, getCpuUsage24h, getCpuUsage7d, getCpuUsage30d } from "../api/prometheus";
 import { getRepoStats } from "../server/github.server";
 import { getNodes } from "../api/nodes";
 
@@ -9,6 +12,7 @@ export const load = async () => {
   async function fetchRepo() {
     return await getRepoStats();
   }
+  await getCpuUsage1h()
   /*
 	async function fetchPosts()  {
 		return await getPosts("1m");

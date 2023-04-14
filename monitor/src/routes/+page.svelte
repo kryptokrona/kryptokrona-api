8e733c26a14e5f1d8ed922641f8afee4da6f7871
appelskrutt34 <ebba1430@hotmail.com>
2023-04-14 21:21:55 +0200

    Prometheus (#151)

--- monitor/src/routes/+page.svelte
+++ monitor/src/routes/+page.svelte
@@ -4,59 +4,123 @@
   import ToggleBox from "../components/toggle-box/ToggleBox.svelte";
   import {
     badRequestsChart,
-    chart1,
-    chart5,
     requestsChart,
     SuccessfulRequestsChart,
   } from "../mock-data/data";
   import TitleAndTextContainer from "../components/containers/TitleAndTextContainer.svelte";
   import Grid from "../components/grids/Grid.svelte";
   import ColumnChart from "../components/charts/ColumnChart.svelte";
-  import AreaChart from "../components/charts/AreaChart.svelte";
-  import GrayButton from "../components/buttons/GrayButton.svelte";
-  import { cpuUsageOverTime } from "../stores/data";
-  import CpuUsageOverTimeChart from "../components/charts/CpuUsageOverTimeChart.svelte";
+  import {
+    cpuUsageOverTime,
+    ramUsageOverTime,
+    diskUsageOverTime,
+    cpuUsage,
+    ramUsage,
+    diskUsage,
+    threads,
+  } from "../stores/data";
+  import { onMount, onDestroy } from "svelte";
+  import {
+    getCpuUsage,
+    getRamUsage,
+    getDiskUsage,
+    getRamUsageOverTime,
+    getCpuUsageOverTime,
+    getDiskUsageOverTime,
+    getThreads,
+  } from "../api/prometheus";
+  import TimeIntervalChart from "../components/charts/TimeIntervalChart.svelte";
 
   export let data;
 
+  $cpuUsage = data.prometheus.cpuUsage;
+  $ramUsage = data.prometheus.ramUsage;
+  $diskUsage = data.prometheus.diskUsage;
   $cpuUsageOverTime = data.prometheus.cpuUsageOverTime;
+  $ramUsageOverTime = data.prometheus.ramUsageOverTime;
+  $diskUsageOverTime = data.prometheus.diskUsageOverTime;
+  $threads = data.prometheus.threads;
+
+  let fiveSecondInterval;
+  let oneSecondInterval;
+  let ramUsageOverTimeInterval = "1h";
+  let cpuUsageOverTimeInterval = "1h";
+  let diskUsageOverTimeInterval = "1h";
+
+  onMount(() => {
+    fiveSecondInterval = setInterval(() => {
+      setRamUsageOverTime();
+      setCpuUsageOverTime();
+      setDiskUsageOverTime();
+    }, 5000);
+    oneSecondInterval = setInterval(() => {
+      setCpuUsage();
+      setRamUsage();
+      setDiskUsage();
+      setThreads();
+    }, 1000);
+  });
+
+  onDestroy(() => {
+    clearInterval(oneSecondInterval);
+    clearInterval(fiveSecondInterval);
+  });
+
+  async function setCpuUsage() {
+    $cpuUsage = await getCpuUsage();
+  }
+  async function setRamUsage() {
+    $ramUsage = await getRamUsage();
+  }
+  async function setDiskUsage() {
+    $diskUsage = await getDiskUsage();
+  }
+  async function setCpuUsageOverTime(event) {
+    if (event) cpuUsageOverTimeInterval = event.detail.time;
+    $cpuUsageOverTime = await getCpuUsageOverTime(cpuUsageOverTimeInterval);
+  }
+  async function setRamUsageOverTime(event) {
+    if (event) ramUsageOverTimeInterval = event.detail.time;
+    $ramUsageOverTime = await getRamUsageOverTime(ramUsageOverTimeInterval);
+  }
+  async function setDiskUsageOverTime(event) {
+    if (event) diskUsageOverTimeInterval = event.detail.time;
+    $diskUsageOverTime = await getDiskUsageOverTime(diskUsageOverTimeInterval);
+  }
+  async function setThreads() {
+    $threads = await getThreads();
+  }
 </script>
 
 <ToggleBox title={"Quick overview"}>
   <Grid columns={5}>
-    <TitleAndTextContainer
-      title="Threads"
-      text={data.prometheus.threads.toString()}
-    />
+    <TitleAndTextContainer title="Threads" text={$threads.toString()} />
     <TitleAndTextContainer
       title="Uptime"
       text={data.prometheus.uptime.toFixed(0) + " days"}
     />
+    <TitleAndTextContainer title="CPU usage" text={$cpuUsage.toFixed(0) + "%"}>
+      <div
+        class="flex h-5/6 items-end justify-center absolute top-7 left-0 right-0"
+      >
+        <CircleChart data={[$cpuUsage]} id="cpuUsage" />
+      </div>
+    </TitleAndTextContainer>
+    <TitleAndTextContainer title="RAM usage" text={$ramUsage.toFixed(0) + "%"}>
+      <div
+        class="flex h-5/6 items-end justify-center absolute top-7 left-0 right-0"
+      >
+        <CircleChart data={[$ramUsage]} id="ramUsage" />
+      </div>
+    </TitleAndTextContainer>
     <TitleAndTextContainer
-      title="CPU usage"
-      text={data.prometheus.cpuUsage.toFixed(0) + "%"}
+      title="DISK usage"
+      text={$diskUsage.toFixed(0) + "%"}
     >
       <div
         class="flex h-5/6 items-end justify-center absolute top-7 left-0 right-0"
       >
-        <CircleChart data={[data.prometheus.cpuUsage]} id="cpuUsage" />
-      </div>
-    </TitleAndTextContainer>
-    <TitleAndTextContainer
-      title="RAM usage"
-      text={data.prometheus.ramUsage.toFixed(0) + "%"}
-    >
-      <div
-        class="flex h-5/6 items-end justify-center absolute top-7 left-0 right-0"
-      >
-        <CircleChart data={[data.prometheus.ramUsage]} id="ramUsage" />
-      </div>
-    </TitleAndTextContainer>
-    <TitleAndTextContainer title="DISK usage" text="40%">
-      <div
-        class="flex h-5/6 items-end justify-center absolute top-7 left-0 right-0"
-      >
-        <CircleChart data={[40]} id="id10" />
+        <CircleChart data={[$diskUsage]} id="diskUsage" />
       </div>
     </TitleAndTextContainer>
   </Grid>
@@ -66,47 +130,30 @@
 
 <ToggleBox title={"History"}>
   <Grid columns={3} gridClass="md-grid">
-    <CpuUsageOverTimeChart />
-
-    <div
-      class={"bg-neutral-200 dark:bg-neutral-800 w-full  rounded-md relative h-64"}
-    >
-      <div class="h-2/5 pt-1 text-center">
-        <p class="pb-1">RAM usage</p>
-        <GrayButton text="1h" />
-        <GrayButton text="24h" />
-        <GrayButton text="7d" />
-        <GrayButton text="30d" />
-      </div>
-      <div class="h-3/5">
-        <AreaChart
-          data={chart5.data}
-          id={chart5.name}
-          tooltipEnabled={true}
-          colors={[COLOR.FUSCHIA]}
-        />
-      </div>
-    </div>
-
-    <div
-      class={"bg-neutral-200 dark:bg-neutral-800 w-full  rounded-md relative h-64"}
-    >
-      <div class="h-2/5 pt-1 text-center">
-        <p class="pb-1">DISK usage</p>
-        <GrayButton text="1h" />
-        <GrayButton text="24h" />
-        <GrayButton text="7d" />
-        <GrayButton text="30d" />
-      </div>
-      <div class="h-3/5">
-        <AreaChart
-          data={chart1.data}
-          id={chart1.name}
-          tooltipEnabled={true}
-          colors={[COLOR.VIOLET]}
-        />
-      </div>
-    </div>
+    <TimeIntervalChart
+      data={[{ data: $cpuUsageOverTime.values, name: "CPU" }]}
+      labels={$cpuUsageOverTime.times}
+      id="cpuUsageOverTime"
+      title="CPU Usage"
+      color={COLOR.BLUE}
+      on:updateTimeInterval={setCpuUsageOverTime}
+    />
+    <TimeIntervalChart
+      data={[{ data: $ramUsageOverTime.values, name: "RAM" }]}
+      labels={$ramUsageOverTime.times}
+      id="ramUsageOverTime"
+      title="RAM Usage"
+      color={COLOR.VIOLET}
+      on:updateTimeInterval={setRamUsageOverTime}
+    />
+    <TimeIntervalChart
+      data={[{ data: $diskUsageOverTime.values, name: "DISK" }]}
+      labels={$diskUsageOverTime.times}
+      id="diskUsageOverTime"
+      title="DISK Usage"
+      color={COLOR.FUSCHIA}
+      on:updateTimeInterval={setDiskUsageOverTime}
+    />
   </Grid>
 </ToggleBox>
 
--- monitor/src/stores/data.js
+++ monitor/src/stores/data.js
@@ -1,4 +1,10 @@
 import { writable } from "svelte/store";
 
 export const nodes = writable([]);
+export const cpuUsage = writable(0);
+export const ramUsage = writable(0);
+export const diskUsage = writable(0);
 export const cpuUsageOverTime = writable({});
+export const ramUsageOverTime = writable({});
+export const diskUsageOverTime = writable({});
+export const threads = writable(0);
